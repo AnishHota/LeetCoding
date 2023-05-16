20c2bb2936e57a13b2772e103764c15174a9b8de
Anish Hota <anishhota92@gmail.com>
2023-05-15 17:02:22 -0700

    Time: 78 ms (6.14%) | Memory: 16.4 MB (14.95%) - LeetSync

---
+++ rotting-oranges/rotting-oranges.py
@@ -0,0 +1,28 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        queue = collections.deque()
+
+        freshO = 0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==2:
+                    queue.append([i,j])
+                if grid[i][j]==1:
+                    freshO +=1
+        
+        levels=0
+        while queue:
+            levels+=1
+            for _ in range(len(queue)):
+                x,y = queue.popleft()
+                for i,j in [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]:
+                    if 0<=i<len(grid) and 0<=j<len(grid[0]) and grid[i][j]==1:
+                        freshO -= 1
+                        grid[i][j]=2
+                        queue.append([i,j])
+        
+        return -1 if freshO!=0 else max(levels-1,0)
