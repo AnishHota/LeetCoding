1de46e532425a7b4f87b51babbca183385ed9370
Anish Hota <anishhota92@gmail.com>
2023-07-15 13:21:38 -0700

    Time: 55 ms (84.45%) | Memory: 18.7 MB (79.62%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        # Maximum height in left subtree + maximum height in right subtree for each node. (Not optimal)
+        # Maximum height in left subtree + maximum height in right subtree for each node.
         self.diam = 0
         def height(root):
             if not root:
@@ -21,19 +21,6 @@ class Solution:
 
         height(root)
         return self.diam
-        
-        # queue = collections.deque()
-        # queue.append(root)
-
-        # while queue:
-        #     node = queue.popleft()
-        #     curr_hei = height(node.left)+height(node.right)
-        #     if curr_hei>max_hei:
-        #         max_hei = curr_hei
-        #     if node.left:
-        #         queue.append(node.left)
-        #     if node.right:
-        #         queue.append(node.right)
 
         return diam
 
