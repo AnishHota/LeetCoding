dd1963e95e06cbe2ce1fe30e463a0d215b6081e5
Anish Hota <anishhota92@gmail.com>
2023-07-15 13:21:13 -0700

    Time: 52 ms (91.51%) | Memory: 18.8 MB (18.23%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -7,27 +7,33 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         # Maximum height in left subtree + maximum height in right subtree for each node. (Not optimal)
-        hei = 1
-        max_hei = 0
+        self.diam = 0
         def height(root):
             if not root:
                 return 0
-            if not root.left and not root.right:
-                return 1
-            return hei + max(height(root.left),height(root.right))
+            left = height(root.left)
+            right = height(root.right)
+
+            if (left+right)>self.diam:
+                self.diam = left+right
+            
+            return 1+max(left,right)
+
+        height(root)
+        return self.diam
         
-        queue = collections.deque()
-        queue.append(root)
+        # queue = collections.deque()
+        # queue.append(root)
 
-        while queue:
-            node = queue.popleft()
-            curr_hei = height(node.left)+height(node.right)
-            if curr_hei>max_hei:
-                max_hei = curr_hei
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
+        # while queue:
+        #     node = queue.popleft()
+        #     curr_hei = height(node.left)+height(node.right)
+        #     if curr_hei>max_hei:
+        #         max_hei = curr_hei
+        #     if node.left:
+        #         queue.append(node.left)
+        #     if node.right:
+        #         queue.append(node.right)
 
-        return max_hei
+        return diam
 
