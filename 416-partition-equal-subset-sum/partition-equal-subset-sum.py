e7b0c37993a7c7cd545c3086ce9ebbde66aca02a
Anish Hota <anishhota92@gmail.com>
2023-07-26 12:32:39 -0700

    Time: 1084 ms (62.81%) | Memory: 16.4 MB (94.69%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -6,23 +6,11 @@ class Solution:
             return False
         
         sumFind = totSum//2
-        dp = [[False for x in range(sumFind+1)] for y in range(len(nums))]
-
-    #1st Column
-        for i in range(len(nums)):
-            dp[i][0] = True
-    
-    #1st row everything false except that number
-        for j in range(sumFind):
-            if j==nums[0]:
-                dp[0][j]=True
-
-        for i in range(len(nums)):
-            for j in range(sumFind+1):
-                if (j-nums[i])>=0:
-                    dp[i][j]=dp[i-1][j] or dp[i-1][j-nums[i]]
-                else:
-                    dp[i][j]=dp[i-1][j]
+        dp = [False] * (sumFind+1)
+        dp[0]=True
+        for num in nums:
+            for i in range(sumFind,num-1,-1):
+                dp[i] = dp[i] or dp[i-num]
         
-        return dp[-1][-1]
+        return dp[-1]
         
