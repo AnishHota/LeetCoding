e5b53a678aeb90ff7d8e8ad2aa0687a4e62a89cb
Anish Hota <anishhota92@gmail.com>
2023-08-20 12:39:27 -0700

    Time: 851 ms (15.80%) | Memory: 64.8 MB (14.93%) - LeetSync

---
+++ 77-combinations/combinations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        comb,curr = [],[]
+        def btrack(i,n,comb,curr):
+            if len(curr)==k:
+                comb.append(curr.copy())
+                return
+            if i>n:
+                return 
+            
+            for j in range(i,n+1):
+                curr.append(j)
+                btrack(j+1,n,comb,curr)
+                curr.pop()
+        
+        btrack(1,n,comb,curr)
+        return comb
