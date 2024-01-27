5d57e21acb24715eb34a9cf13a933cd949dab4b3
Anish Hota <anishhota92@gmail.com>
2024-01-27 15:54:00 -0700

    Time: 90 ms (81.85%) | Memory: 16.7 MB (59.46%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -10,18 +10,13 @@ class Solution:
                     print(board[i][j])
                     if track[board[i][j]]:
                         if (i in track[board[i][j]][0]) or (j in track[board[i][j]][1]):
-                            # print(board[i][j])
-                            # print(i,track[board[i][j]][0])
-                            # print(j,track[board[i][j]][1])
                             return False
-                        if int(int((i/3))*3+int(j/3)) in track[board[i][j]][2]:
-                            # print(board[i][j],i,j)
-                            # print(int(int((i/3))*3+int(j/3)),track[board[i][j]][2])
+                        if ((i//3)*3+(j//3)) in track[board[i][j]][2]:
                             return False
                         track[board[i][j]][0].append(i)
                         track[board[i][j]][1].append(j)
-                        track[board[i][j]][2].append(int(int((i/3))*3+int(j/3)))
+                        track[board[i][j]][2].append((i//3)*3+(j//3))
                     else:
-                        track[board[i][j]] = [[i],[j],[int(int((i/3))*3+int(j/3))]]
+                        track[board[i][j]] = [[i],[j],[(i//3)*3+(j//3)]]
         
         return True
