50a6c308a5e6007c2fd6948893f8db99147119a3
Anish Hota <anishhota92@gmail.com>
2024-02-06 20:50:21 -0700

    Time: 267 ms (73.50%) | Memory: 18.2 MB (59.01%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -3,17 +3,15 @@ class Solution:
         
         l = 1
         r = max(piles)
-        ans = float(inf)
 
         while l<=r:
             k = int((r+l)/2)
             hrs = 0
             for p in piles:
-                hrs += int(p/k) + (1 if p%k!=0 else 0)
+                hrs += math.ceil(p/k)
             if hrs<=h:
-                ans = min(ans,k)
                 r=k-1
             else:
                 l=k+1
 
-        return ans
+        return l
