5276981cb3296766802791b1f51d1886dc41d12f
Anish Hota <anishhota92@gmail.com>
2024-03-01 12:06:38 -0700

    Time: 82 ms (86.76%) | Memory: 19.6 MB (81.54%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,11 +3,8 @@ class Solution:
         dic = defaultdict(list)
 
         for s in strs:
-            arr = [-1 for _ in range(27)]
-            for c in s:
-                arr[ord(c)-97]+=1
-            temp = ''.join([str(elem) for elem in arr])
-            dic[temp].append(s)
+            arr = ''.join(sorted(s))
+            dic[arr].append(s)
 
         result = []
         for x in dic.values():
