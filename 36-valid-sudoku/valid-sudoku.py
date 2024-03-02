05bc09ffeebec4c13667c25828984c6df1e82e99
Anish Hota <anishhota92@gmail.com>
2024-03-01 17:39:48 -0700

    Time: 88 ms (80.50%) | Memory: 16.6 MB (75.60%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,22 +1,20 @@
-from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [[] for _ in range(9)]
+        cols = [[] for _ in range(9)]
+        box = [[] for _ in range(9)]
 
-        track = defaultdict(list)
-        r, c = 9,9
-        for i in range(r):
-            for j in range(c):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
                 if board[i][j]!=".":
-                    print(board[i][j])
-                    if track[board[i][j]]:
-                        if (i in track[board[i][j]][0]) or (j in track[board[i][j]][1]):
-                            return False
-                        if ((i//3)*3+(j//3)) in track[board[i][j]][2]:
-                            return False
-                        track[board[i][j]][0].append(i)
-                        track[board[i][j]][1].append(j)
-                        track[board[i][j]][2].append((i//3)*3+(j//3))
+                    if board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in box[(i//3)*3+(j//3)]:
+                        return False
                     else:
-                        track[board[i][j]] = [[i],[j],[(i//3)*3+(j//3)]]
+                        rows[i].append(board[i][j])
+                        cols[j].append(board[i][j])
+                        box[(i//3)*3+(j//3)].append(board[i][j])
         
         return True
+
+
+        
