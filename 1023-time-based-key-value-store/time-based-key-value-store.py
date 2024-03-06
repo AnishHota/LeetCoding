c1181227bc2d64d2dee0ba330351adf1cede65cd
Anish Hota <anishhota92@gmail.com>
2024-03-06 10:47:49 -0700

    Time: 628 ms (16.72%) | Memory: 74.2 MB (55.78%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -4,11 +4,8 @@ class TimeMap:
     def __init__(self):
         self.timemap = defaultdict(list)
 
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        if key in self.timemap:       
-            self.timemap[key].append((timestamp,value))
-        else:
-            self.timemap[key]=[(timestamp,value)]
+    def set(self, key: str, value: str, timestamp: int) -> None:    
+        self.timemap[key].append((timestamp,value))
 
     def get(self, key: str, timestamp: int) -> str:
         ans = ""
