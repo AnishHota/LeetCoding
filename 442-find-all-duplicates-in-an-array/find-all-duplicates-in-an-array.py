90a8f5903e1b6c1ea3e4516abfd8758e848ff506
Anish Hota <anishhota92@gmail.com>
2024-03-24 22:58:19 -0700

    Time: 276 ms (35.27%) | Memory: 24.9 MB (49.65%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -4,7 +4,8 @@ class Solution:
         for i,x in enumerate(nums):
             if nums[abs(x)-1]<0:
                 ans.append(abs(x))
-            nums[abs(x)-1]*=-1
+            else:
+                nums[abs(x)-1]*=-1
         
         return ans
         
