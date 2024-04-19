2064741aef023bcc3a922331c731b643f86a43b5
Anish Hota <anishhota92@gmail.com>
2024-04-18 21:08:43 -0700

    Time: 211 ms (99.11%) | Memory: 18.9 MB (61.54%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,27 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        r,c = len(grid),len(grid[0])
-        def dfs(grid,i,j):
-            if i>=0 and i<r and j>=0 and j<c:
-                if grid[i][j]=='1':
-                    grid[i][j]='2'
-                    dfs(grid,i-1,j)
-                    dfs(grid,i,j-1)
-                    dfs(grid,i+1,j)
-                    dfs(grid,i,j+1)
-                else:
-                    return
-            else:
-                return
-        
-        count = 0
-        for x in range(r):
-            for y in range(c):
-                if grid[x][y]=='1':
-                    count+=1
-                    dfs(grid,x,y)
-        
-        return count
+        rows, cols = len(grid), len(grid[0])
 
-        
+        def bfs(grid,i,j):
+            q = deque()
+            q.append((i,j))
+            while q:
+                a,b = q.popleft()
+                grid[a][b]='x'
+                if a-1>=0 and grid[a-1][b]=='1':
+                    grid[a-1][b]='x'
+                    q.append((a-1,b))
+                if a+1<rows and grid[a+1][b]=='1':
+                    grid[a+1][b]='x'
+                    q.append((a+1,b))
+                if b-1>=0 and grid[a][b-1]=='1':
+                    grid[a][b-1]='x'
+                    q.append((a,b-1))
+                if b+1<cols and grid[a][b+1]=='1':
+                    grid[a][b+1]='x'
+                    q.append((a,b+1))
+        cnt =0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]=="1":
+                    cnt+=1
+                    bfs(grid,i,j)
+        return cnt
