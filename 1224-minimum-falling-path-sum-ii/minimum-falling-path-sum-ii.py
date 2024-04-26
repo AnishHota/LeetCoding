ac39ae7154225599bf84277f6d465700ea81d226
Anish Hota <anishhota92@gmail.com>
2024-04-26 11:08:57 -0700

    Time: 225 ms (77.88%) | Memory: 19.9 MB (92.04%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.py
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.py
@@ -2,9 +2,27 @@ class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
 
+
+        def find_min(arr):
+            dp = []
+            for ind,x in enumerate(arr):
+                if len(dp)<2:
+                    dp.append((x,ind))
+                    dp.sort()
+                elif x<=dp[1][0]:
+                    dp.pop()
+                    dp.append((x,ind))
+                    dp.sort()
+            return dp
+        
+        dp = find_min(grid[rows-1])
+
         for i in range(rows-2,-1,-1):
             for j in range(cols-1,-1,-1):
-                temp_arr = grid[i+1][:j]+grid[i+1][j+1:]
-                grid[i][j]+=min(temp_arr)
+                if j!=dp[0][1]:
+                    grid[i][j]+=dp[0][0]
+                else:
+                    grid[i][j]+=dp[1][0]
+            dp = find_min(grid[i])
         
         return min(grid[0])
