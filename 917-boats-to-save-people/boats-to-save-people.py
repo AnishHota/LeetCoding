808e306796691476f713d1c45a7bddf1742a907f
Anish Hota <anishhota92@gmail.com>
2024-05-03 17:18:16 -0700

    Time: 354 ms (40.82%) | Memory: 23.5 MB (8.54%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -3,6 +3,9 @@ class Solution:
         people = sorted(people)
         boats = 0
         i,j = 0, len(people)-1
+        while people[j]>=limit:
+            boats+=1
+            j-=1
         while i<=j:
             if people[j] < limit and (people[i]+people[j]) <= limit:
                 boats+=1
