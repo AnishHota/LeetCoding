1c8ca19babe29f4032ab0c8ec5440969ae0b6acc
Anish Hota <anishhota92@gmail.com>
2024-05-07 11:49:52 -0700

    Time: 210 ms (96.71%) | Memory: 19.4 MB (64.32%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
@@ -3,34 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-sys.set_int_max_str_digits(100000)
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        number = 0
-        temp = head
-        while temp:
-            number = number*10+temp.val
-            temp = temp.next
+        ans = head
+        if head and head.val>4:
+            node = ListNode(1,head)
+            ans = node
         
-        number *= 2
-        number = list(str(number))
-        i=0
-        temp = head
-        while temp:
-            temp.val = int(number[i])
-            i+=1
-            if temp.next:
-                temp = temp.next
-            else:
-                break
-            
+        while head:
+            head.val = (head.val*2)%10 
+            if head.next and head.next.val>4:
+                head.val+=1
+            head = head.next
+
+        return ans     
+
         
-        while i<len(number):
-            node = ListNode(int(number[i]))
-            temp.next = node
-            temp = temp.next
-            i+=1
-        
-        temp.next = None
-        return head
