11166b2ad6d6a560356c9d37a402eff366566541
Anish Hota <anishhota92@gmail.com>
2024-05-11 22:50:58 -0700

    Time: 155 ms (12.77%) | Memory: 17.1 MB (28.98%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
@@ -6,8 +6,8 @@ class Solution:
         ans = [[0 for _ in range(rows-2)] for _ in range(cols-2)]
         for i in range(rows-2):
             for j in range(cols-2):
-                for x in range(3):
-                    for y in range(3):
-                        ans[i][j] = max(ans[i][j], grid[i+x][y+j])
+                for x in range(i,i+3):
+                    for y in range(j,j+3):
+                        ans[i][j] = max(ans[i][j], grid[x][y])
         
         return ans
