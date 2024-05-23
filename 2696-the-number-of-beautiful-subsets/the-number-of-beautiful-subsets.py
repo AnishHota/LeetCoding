855c0cd64e5fc844ad92766e200bf47b577047ff
Anish Hota <anishhota92@gmail.com>
2024-05-23 13:20:13 -0700

    Time: 2722 ms (57.70%) | Memory: 16.7 MB (28.09%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.py
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.py
@@ -2,18 +2,14 @@ class Solution:
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
         
         self.ans = 0
-        nums.sort()
-        cnt = [0] * (nums[-1]+1)
 
-        def sub(ind,curr):
+        def sub(ind, cnt):
             for i in range(ind,len(nums)):
-                if (nums[i]-k)<=0 or cnt[nums[i]-k]==0:
+                if cnt[nums[i]-k]==0 and cnt[nums[i]+k]==0:
                     self.ans+=1
-                    curr.append(nums[i])
                     cnt[nums[i]]+=1
-                    sub(i+1,curr)
+                    sub(i+1,cnt)
                     cnt[nums[i]]-=1
-                    curr.pop()
         
-        sub(0,[])
+        sub(0,defaultdict(int))
         return self.ans
