9207b3ef62b9b1ee8dfb0b39e12cfd4688c0e786
Anish Hota <anishhota92@gmail.com>
2024-06-12 10:06:30 -0700

    Time: 26 ms (98.42%) | Memory: 16.5 MB (33.92%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,18 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0
-        right = len(nums)-1
+        n = len(nums)-1
+        l,r = 0,len(nums)-1
         i = 0
-
-        while i<=right and left<right:
+        while i<=r and l<=r:
             if nums[i]==0:
-                nums[left],nums[i] = nums[i],nums[left]
-                left+=1
+                nums[i],nums[l] = nums[l],nums[i]
+                l+=1
                 i+=1
             elif nums[i]==2:
-                nums[right],nums[i] = nums[i],nums[right]
-                right-=1
+                nums[i],nums[r] = nums[r],nums[i]
+                r-=1
             else:
                 i+=1
-            
