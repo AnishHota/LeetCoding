ef596a9fe0be9dd8c34b7e75e0f839ebdc976018
Anish Hota <anishhota92@gmail.com>
2024-06-28 00:17:16 -0700

    Time: 1285 ms (38.53%) | Memory: 42.6 MB (48.36%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
@@ -1,18 +1,17 @@
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
-        graph = defaultdict(list)
+        graph = defaultdict(int)
 
         for x,y in roads:
-            graph[x].append(y)
-            graph[y].append(x)
+            graph[x]+=1
+            graph[y]+=1
         
-        graph = dict(sorted(graph.items(), key = lambda x: len(x[1]), reverse=True))
+        graph = dict(sorted(graph.items(), key = lambda x: x[1], reverse=True))
         
-        for x,y in graph.items():
-            graph[x]=n
-            n-=1
         ans = 0
-        for x,y in roads:
-            ans += graph[x]+graph[y]
+        for x,y in graph.items():
+            ans += graph[x]*n
+            n-=1
+        
 
         return ans
