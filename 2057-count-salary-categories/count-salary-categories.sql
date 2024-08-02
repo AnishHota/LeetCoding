4c5268918921e62bcd46e6f623a750be11ca1ee7
Anish Hota <anishhota92@gmail.com>
2024-08-01 19:50:15 -0700

    Time: 2393 ms (11.54%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -2,11 +2,11 @@
 SELECT 'High Salary' AS category,COALESCE(COUNT(*),0) AS accounts_count
 FROM Accounts
 WHERE income>50000
-UNION ALL
+UNION 
 SELECT 'Low Salary' AS category,COALESCE(COUNT(*),0) AS accounts_count
 FROM Accounts
 WHERE income<20000
-UNION ALL
+UNION 
 SELECT 'Average Salary' AS category,COALESCE(COUNT(*),0) AS accounts_count
 FROM Accounts
 WHERE income>=20000 AND income<=50000
