21f70e52408a91517bd33f2f04535d115521659f
Anish Hota <anishhota92@gmail.com>
2024-08-01 20:16:32 -0700

    Time: 1305 ms (27.29%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -18,7 +18,7 @@ SELECT B.title AS results FROM
     FROM MovieRating mr
     INNER JOIN Movies m
     ON mr.movie_id = m.movie_id
-    AND YEAR(mr.created_at) = "2020" AND MONTH(mr.created_at)="02"
+    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
     GROUP BY mr.movie_id
     ORDER BY avg_rating DESC, m.title ASC
     LIMIT 1
