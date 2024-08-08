30b38035eff398dc044fe16a1a2968ecfd1efd88
Anish Hota <anishhota92@gmail.com>
2024-08-08 11:02:31 -0700

    Time: 235 ms (72.40%) | Memory: 18.9 MB (74.75%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,30 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        rows, cols = len(grid), len(grid[0])
+        r,c = len(grid), len(grid[0])
 
-        def bfs(grid,i,j):
+        def dfs(grid,i,j):
+            di = [[0,1],[1,0],[0,-1],[-1,0]]
             q = deque()
-            q.append((i,j))
+            q.append([i,j])
+
             while q:
                 a,b = q.popleft()
-                grid[a][b]='x'
-                if a-1>=0 and grid[a-1][b]=='1':
-                    grid[a-1][b]='x'
-                    q.append((a-1,b))
-                if a+1<rows and grid[a+1][b]=='1':
-                    grid[a+1][b]='x'
-                    q.append((a+1,b))
-                if b-1>=0 and grid[a][b-1]=='1':
-                    grid[a][b-1]='x'
-                    q.append((a,b-1))
-                if b+1<cols and grid[a][b+1]=='1':
-                    grid[a][b+1]='x'
-                    q.append((a,b+1))
-        cnt =0
-        for i in range(rows):
-            for j in range(cols):
+                for x,y in di:
+                    if (a+x>=0 and a+x<r) and (b+y>=0 and b+y<c) and (grid[a+x][b+y]=="1"):
+                        grid[a+x][b+y]="0"
+                        q.append([a+x,b+y])
+
+        ans = 0
+        for i in range(r):
+            for j in range(c):
                 if grid[i][j]=="1":
-                    cnt+=1
-                    bfs(grid,i,j)
-        return cnt
+                    ans +=1
+                    dfs(grid,i,j)
+        
+        return ans
