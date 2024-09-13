53095ab0a6e2bee6936404b3bf0e75247ad37bd1
Anish Hota <anishhota92@gmail.com>
2024-09-13 08:11:29 -0700

    Time: 313 ms (23.78%) | Memory: 31.4 MB (10.11%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
@@ -1,19 +1,14 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
         prexor = [arr[0]]
-        sufxor = [arr[-1]]
         n = len(arr)
         for i in range(1,n):
             prexor.append(prexor[-1]^arr[i])
-            sufxor.append(sufxor[-1]^arr[n-i-1])
-        sufxor = sufxor[::-1]
-        total = prexor[-1]
+
         ans = []
         for x,y in queries:
-            temp = total
+            temp = prexor[y]
             if x>0:
                 temp = temp ^ prexor[x-1]
-            if y<len(arr)-1:
-                temp = temp ^ sufxor[y+1]
             ans.append(temp)
         return ans
