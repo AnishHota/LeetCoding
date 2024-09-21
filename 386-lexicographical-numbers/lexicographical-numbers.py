dfe47fbeed58ecf2e9e27c87c9039fa4c92e8fcf
Anish Hota <anishhota92@gmail.com>
2024-09-21 09:14:23 -0700

    Time: 146 ms (12.80%) | Memory: 25.1 MB (9.09%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -6,19 +6,15 @@ class Solution:
         def dfs(i):
             if i>n:
                 return
-            
+            self.ans.append(i)
             for x in range(10):
                 temp = i*10+x
-                if temp<=n:
-                    self.ans.append(temp)
-                    dfs(temp)
-                else:
-                    return
+                dfs(temp)
             
             return
         
         end = min(n+1,10)
         for i in range(1,end):
-            self.ans.append(i)
+            # self.ans.append(i)
             dfs(i)
         return self.ans
