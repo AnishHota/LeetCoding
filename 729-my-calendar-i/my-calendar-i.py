7c3551b31ebd2ff3534e034a15bec127132282cd
Anish Hota <anishhota92@gmail.com>
2024-09-26 08:52:40 -0700

    Time: 154 ms (92.53%) | Memory: 17.3 MB (65.25%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -1,15 +1,34 @@
+class TreeNode:
+    def __init__(self,s,e):
+        self.s = s
+        self.e = e
+        self.l = None
+        self.r = None
+
 class MyCalendar:
 
     def __init__(self):
-        self.calendar = []
+        self.root = None
 
     def book(self, start: int, end: int) -> bool:
-        for i,(x,y) in enumerate(self.calendar):
-            if (start>=x and start<y) or (end>x and end<y) or (x>=start and x<end) or (y>start and y<end):
-                return False
-        self.calendar.append([start,end])
-        return True
-
+        if not self.root:
+            self.root = TreeNode(start,end)
+            return True
+        return self.insert(self.root,start,end)
+    
+    def insert(self,node,start,end):
+        if start>=node.e:
+            if node.r:
+                return self.insert(node.r,start,end)
+            node.r = TreeNode(start,end)
+            return True
+        elif end<=node.s:
+            if node.l:
+                return self.insert(node.l,start,end)
+            node.l = TreeNode(start,end)
+            return True
+        else:
+            return False
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
