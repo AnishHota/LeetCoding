556ed1a8f1c40217170d939878fe2d41e9e4d4e9
Anish Hota <anishhota92@gmail.com>
2024-09-30 08:22:48 -0700

    Time: 75 ms (82.15%) | Memory: 17.5 MB (58.60%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.py
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.py
@@ -3,21 +3,26 @@ class CustomStack:
     def __init__(self, maxSize: int):
         self.arr = []
         self.ms = maxSize
+        self.inc =[0]*maxSize
 
     def push(self, x: int) -> None:
         if len(self.arr)<self.ms:
             self.arr.append(x)
 
     def pop(self) -> int:
-        if self.arr:
-            return self.arr.pop()
-        else:
+        if not self.arr:
             return -1
+        ind = len(self.arr)-1
+        res = self.arr.pop()+self.inc[ind]
+        if ind>0:
+            self.inc[ind-1] += self.inc[ind]
+        self.inc[ind] = 0
+        return res
 
     def increment(self, k: int, val: int) -> None:
-        n = min(len(self.arr),k)
-        for i in range(n):
-            self.arr[i]+=val
+        n = min(len(self.arr)-1,k-1)
+        if n>=0:
+            self.inc[n] += val
 
 
 # Your CustomStack object will be instantiated and called as such:
