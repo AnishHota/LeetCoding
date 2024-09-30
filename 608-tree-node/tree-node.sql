fb19bf08e13a7fc71b1d8464fa6f479aac62a459
Anish Hota <anishhota92@gmail.com>
2024-09-30 09:24:58 -0700

    Time: 549 ms (19.14%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,15 +1,6 @@
 # Write your MySQL query statement below
-
-WITH CTE AS
-(
-    SELECT DISTINCT a.id
-    FROM Tree a 
-    INNER JOIN Tree b
-    WHERE a.id = b.p_id
-    AND a.p_id IS NOT NULL
-)
 SELECT id,
-IF(p_id IS NULL,'Root',IF(id IN (SELECT id FROM CTE),'Inner','Leaf')) AS type
+IF(p_id IS NULL,'Root',IF(id IN (SELECT p_id FROM TREE),'Inner','Leaf')) AS type
 FROM
 Tree
 
