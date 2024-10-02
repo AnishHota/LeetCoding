5ce92a8c96873757777b0340acc1ef830d72b41e
Anish Hota <anishhota92@gmail.com>
2024-10-02 09:39:24 -0700

    Time: 1016 ms (82.51%) | Memory: 62.8 MB (24.14%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -7,7 +7,5 @@ class Solution:
             if p[0]>curr:
                 res+=1
                 curr = p[1]
-            else:
-                curr = min(curr,p[1])
         
         return res+1
