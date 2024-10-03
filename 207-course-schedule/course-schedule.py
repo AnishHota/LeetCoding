c9c21218462f3ff57ac22fce38dbd2b4d9709f63
Anish Hota <anishhota92@gmail.com>
2024-10-02 21:10:45 -0700

    Time: 107 ms (16.68%) | Memory: 18.4 MB (39.97%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,33 +1,22 @@
-from collections import defaultdict
-from collections import deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        inDegree = [0]*numCourses
+        for c,pre in prerequisites:
+            graph[pre].append(c)
+            inDegree[c]+=1
         
-        graph = defaultdict(set)
-        inEdges = [0]*numCourses
-        def makeGraph(prereqs):
-
-            for x in prereqs:
-                graph[x[1]].add(x[0])
-                inEdges[x[0]]+=1
-        
-        makeGraph(prerequisites)
-        queue = deque()
-        for i in range(numCourses):
-            if inEdges[i]==0:
-                queue.append(i)
-
-        topSort = []
-        while queue:
-            node = queue.popleft()
-            topSort.append(node)
+        q = deque([i for i in range(len(inDegree)) if inDegree[i]==0])
+        visited = set()
+        while q:
+            node = q.popleft()
+            if node in visited:
+                continue
+            visited.add(node)
             for neigh in graph[node]:
-                inEdges[neigh]-=1
-                if inEdges[neigh]==0:
-                    queue.append(neigh)
+                inDegree[neigh]-=1
+                if inDegree[neigh]==0:
+                    q.append(neigh)
 
-        if len(topSort)!=numCourses:
-            return False
-        return True
-
+        return len(visited)==numCourses
         
