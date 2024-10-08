d540d98354525dcc88a80858834a7c679b9877ef
Anish Hota <anishhota92@gmail.com>
2024-10-08 08:30:39 -0700

    Time: 296 ms (44.83%) | Memory: 27.9 MB (45.06%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,24 +1,27 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
-        q = list(s)
         cnto=0
         cntc=0
         j = len(s)-1
-        while j>=0 and q[j]!='[':
+        while j>=0 and s[j]!='[':
             j-=1
         res=0
+        if not s:
+            return res
+        if j<len(s)//2:
+            return res
 
-        for i,x in enumerate(q):
+        for i,x in enumerate(s):
             if x=='[':
                 cnto+=1
-            if x==']':
+            elif x==']':
                 cntc+=1
             if cntc>cnto and i<j:
-                    q[i],q[j]=q[j],q[i]
                     cntc-=1
                     cnto+=1
                     res+=1
-                    while j>=0 and q[j]!='[':
+                    j-=1
+                    while j>=0 and s[j]!='[':
                         j-=1
 
         return res
