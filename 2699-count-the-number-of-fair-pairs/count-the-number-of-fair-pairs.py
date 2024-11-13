bb017cd7d916455b09e1a64224c27b7a512090cb
Anish Hota <anishhota92@gmail.com>
2024-11-13 10:03:04 -0700

    Time: 187 ms (69.05%) | Memory: 30.5 MB (91.73%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        nums.sort()
+
+        def countpairs(val):
+            cnt = 0
+            j = len(nums)-1
+
+            for i in range(len(nums)):
+                while i<j and nums[i]+nums[j]>val:
+                    j-=1
+                cnt += max(0,j-i)
+            return cnt
+        
+        return countpairs(upper) - countpairs(lower-1)
+
+
+
